c49a7b6deb8aa4da2706eeaad77d9b8ce8eceafd
John Argote <argote.john@gmail.com>
2024-03-22 22:25:59 -0500

    rendered articles using array of objects

---
+++ js/blogs.js
@@ -0,0 +1,28 @@
+const blogs = [
+    {
+        uuid: crypto.randomUUID(),
+        image: "./images/image1.jpg",
+        alt: "Image of an open computer on a desk with lines of code",
+        date: "July, 23, 2023",
+        title: "Blog one",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+    {
+        uuid: crypto.randomUUID(),
+        image: "./images/image2.jpg",
+        alt: "A computer on a desk",
+        date: "July, 23, 2023",
+        title: "Blog two",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    },
+    {
+        uuid: crypto.randomUUID(),
+        image: "./images/image3.jpg",
+        alt: "A computer on a desk",
+        date: "July, 23, 2023",
+        title: "Blog two",
+        content: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path."
+    }
+]
+
+export { blogs }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
+import { blogs } from "./blogs.js"
+
+// console.log(blogs)
+
+// blogs.forEach(blog => {
+//     console.log(blog)
+//     // articleHtml(blog)
+
+// });
+
+
+// menu: hamburger
 document.getElementById("hamburger-menu").addEventListener("click", toggleMenu)
 
 function toggleMenu() {
     document.getElementById("nav").classList.toggle("hidden")
 }
+// end menu: hamburger
+
+// articles
+let html = ""
+
+blogs.forEach(blog => {
+    html += articleHtml(blog)
+})
+
+function articleHtml(blog) {
+    const { image, alt, date, title, content } = blog
+    return `<article>
+        <img class="blog-img" src="${image}" alt="${alt}">
+        <span>${date}</span>
+        <h2 class="blog-title">${title}</h2>
+        <p class="blog-text">${content}</p>
+    </article>`
+}
+
+renderArticles()
+function renderArticles() {
+    document.getElementById("articles-container").innerHTML = html
+}
+
+// end articles
